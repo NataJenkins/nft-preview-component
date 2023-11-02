7f07377408cf17cba50ebd975c80a996dffd657a
Natalia Macias <natajenkins@hotmail.com>
2023-11-02 16:11:30 -0600

    Nft card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import NFTImage from "./assets/images/image-equilibrium.jpg";
+import Avatar from "./assets/images/image-avatar.png";
+import EthIcon from "./assets/images/icon-ethereum.svg";
+import ClockIcon from "./assets/images/icon-clock.svg";
+import EyeIcon from "./assets/images/icon-view.svg";
+import "./App.scss";
 function App() {
-    return <>APP</>;
+    return (
+        <div className="card-container">
+            <div className="image-container">
+                <img src={NFTImage} alt="Equilibrium" className="nft-image" />
+                <img src={EyeIcon} alt="" className="icon" />
+            </div>
+            <h1 className="title">Equilibrium #3429</h1>
+            <h2 className="description">
+                Our Equilibrium collection promotes balance and calm.
+            </h2>
+            <div className="details">
+                <h4 className="price">
+                    {" "}
+                    <img src={EthIcon} alt="" className="icon" /> 0.041 ETH
+                </h4>
+                <h4 className="time-stamp">
+                    {" "}
+                    <img src={ClockIcon} alt="" className="icon" /> 3 days left
+                </h4>
+            </div>
+            <div className="author">
+                <img src={Avatar} alt="" />
+                <p>
+                    Creation of <span>Jules Wyvern</span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
